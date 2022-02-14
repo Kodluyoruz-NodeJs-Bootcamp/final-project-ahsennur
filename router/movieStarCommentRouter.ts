904ac3ae2c15dae77a481f0cf59f5f691052b0ea
Ahsen Turksoy <ahsen@tryoto.com>
2022-02-14 07:38:49 +0300

    deleting wrong push

--- router/movieStarCommentRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { createMovieStarComment } from '../controller/movieStarCommentController';
-import { auth } from '../middleware/auth';
-const router = Router();
-
-//router for movieStar comments
-router.put('/:id', auth, createMovieStarComment);
-
-export default router;
